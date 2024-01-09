ca2b29182f608aeac2d3d89b2e134b87e71fd5f2
Changwhi <whinada@gmail.com>
2024-01-09 13:48:51 -0800

    feat: sets teh dynamicParams

--- app/[meetupId]/page.tsx
+++ app/[meetupId]/page.tsx
@@ -1,34 +1,39 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
+// it is used to define behaviour of a page that isn't pre rendered at build time.
+// (dynamic sgement)
+// if you set it to true, which is default, it will show a fallback page
+// while the page is being generated, 'false' shows you 404 page.
 
-export default async function MeetupDetails({params}: any) {
-  const meetupdetail = await getPost(params)
-  const { id } = params
-  return (
-    <MeetupDetail
-      image={meetupdetail.image}
-      title={meetupdetail.title}
-      address={meetupdetail.address}
-      description={id}
-
-    />
-  );
-}
+export const dynamicParams = false;
 
 export async function generateStaticParams(){
-    return [{ id: 'm1'}]
+    return [{ meetupId: 'm1'}]
 }
 
-async function getPost(context: any) {
-  const meetupid = context.meetupId;
+async function getPost(meetupId: string) {
 
-  console.log(context.meetupId)
+  console.log(meetupId)
   return ({
       image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/640px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg",
       title:"first",
       address:"some street 5",
       description:"thisis",
-      id:meetupid
+      id:meetupId
 });
 }
+export default async function MeetupDetails({params}:{ params:{meetupId: string}}) {
+  const { meetupId } = params
+  const meetupdetail = await getPost(meetupId)
+  return (
+    <MeetupDetail
+      image={meetupdetail.image}
+      title={meetupdetail.title}
+      address={meetupdetail.address}
+      description={meetupdetail.id}
+
+    />
+  );
+}
+
