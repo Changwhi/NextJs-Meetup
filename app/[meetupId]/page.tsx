f0e6fcdd2d22c7229d12433a7891f57a1c12088a
Changwhi <whinada@gmail.com>
2024-01-10 00:51:21 -0800

    feat: implements dinamic detail page

--- app/[meetupId]/page.tsx
+++ app/[meetupId]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 // it is used to define behaviour of a page that isn't pre rendered at build time.
 // (dynamic sgement)
 // if you set it to true, which is default, it will show a fallback page
@@ -8,45 +8,44 @@ import { MongoClient } from "mongodb";
 
 export const dynamicParams = false;
 
-
 async function getPost(meetupId: string) {
-
-
-  console.log(meetupId)
-  return ({
-  image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/640px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg",
-      title:"first",
-      address:"some street 5",
-      description:"thisis",
-      id:meetupId
-});
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.h59cv.mongodb.net/?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const SelectedMeetups = await meetupsCollection.findOne({ _id: new ObjectId(meetupId) });
+  client.close();
+  return SelectedMeetups;
 }
 
-export async function generateStaticParams(){
+export async function generateStaticParams() {
   const client = await MongoClient.connect(
     `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.h59cv.mongodb.net/?retryWrites=true&w=majority`
   );
   const db = client.db();
 
-  const meetupsCollection = db.collection('meetups');
-  const meetups = await meetupsCollection.find({_id: 1}).toArray();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({ _id: 1 }).toArray();
 
+  client.close();
 
-
-    return meetups.map(meetup => ([{meetupId: meetup._id.toString()}]))
+  return meetups.map((meetup) => [{ meetupId: meetup._id.toString() }]);
 }
 
-export default async function MeetupDetails({params}:{ params:{meetupId: string}}) {
-  const { meetupId } = params
-  const meetupdetail = await getPost(meetupId)
+export default async function MeetupDetails({
+  params,
+}: {
+  params: { meetupId: string };
+}) {
+  const { meetupId } = params;
+  const meetupdetail: any = await getPost(meetupId);
   return (
     <MeetupDetail
       image={meetupdetail.image}
       title={meetupdetail.title}
       address={meetupdetail.address}
-      description={meetupdetail.id}
-
+      description={meetupdetail._id.toString()}
     />
   );
 }
-
