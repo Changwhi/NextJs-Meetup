c9bfe4d12636b0420dd96d4da1de2a40c3697e40
Changwhi <whinada@gmail.com>
2024-01-09 23:26:16 -0800

    feat: fetches data from Mongo db

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,5 @@
-import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'first meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/1024px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg',
-    address: "Vancouver BC CANADA",
-    description: "this is 1 meetup"
-  },
-  {
-    id: 'm2',
-    title: '2nd meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/640px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg',
-    address: "Vancouver BC CANADA",
-    description: "this is 2 meetup"
-  },
-  {
-    id: 'm3',
-    title: '3rd meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/640px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg',
-    address: "Vancouver BC CANADA",
-    description: "this is 3 meetup"
-  },
-  {
-    id: 'm4',
-    title: '4th meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg/640px-Sonoma_chipmunk_at_Samuel_P._Taylor_State_Park.jpg',
-    address: "Vancouver BC CANADA",
-    description: "this is 4 meetup"
-  },
-]
-
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 interface HomeProps {
   meetups: {
     id: string;
@@ -38,27 +7,37 @@ interface HomeProps {
     image: string;
     address: string;
     description: string;
-  }
-  
+  };
 }
 export default async function Home() {
-  const meetups = await getProps()
-  return (
-    <MeetupList meetups={meetups}> </MeetupList>
-    )
+  const meetups = await getProps();
+  return <MeetupList meetups={meetups}> </MeetupList>;
 }
 
 async function getProps() {
+  // the code inside of this function will not be stored in the client side.
+  const client = await MongoClient.connect(
+    "mongodb+srv://changwhi:102030@cluster0.h59cv.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
 
+  const meetups = await db.collection("meetups").find().toArray();
+  client.close();
   //Async, fetch data before rendering Home page. And pass that props to Home component.
   //So, Home component can be pre rendered with fetched data
-   
-  //To update fetched data. 
-  //becaues this function will pass the data while it builts. So after building this 
+
+  //To update fetched data.
+  //becaues this function will pass the data while it builts. So after building this
   //website, if you update data, it does not apply to the website. to apply new data to your website,
   //You should redploy the website, but it will handle it without redploying if you use revalidate.
-  
+
   //const res = await fetch(`https://.../posts`, { next: { revalidate: 60 } })
 
-    return DUMMY_MEETUPS
+  return meetups.map((meetup) => ({
+    key: meetup._id.toString(),
+    id: meetup._id.toString(),
+    image: meetup.image,
+    title: meetup.title,
+    address: meetup.address,
+  }));
 }
